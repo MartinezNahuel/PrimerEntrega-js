c8fcde2d72541f4c4a02b3d09c535f5cafe93eef
Martinez Nahuel <nahuel.ivan.martinez@hotmail.com>
2023-01-18 15:02:11 -0300

    nuevo proyecto iphone

--- js/main.js
+++ js/main.js
@@ -1,44 +1,38 @@
-
-const tna = 0.75; 
-const año = 365; 
-let montoDeInversion;
-let plazoNumero;
-
-do{
-montoDeInversionIngresado=prompt('Ingrese un monto de inversion mayor o igual a 1000 o presione ESC para finalizar')
-const montoDeInversion=parseFloat(montoDeInversionIngresado)
-if(montoDeInversion>=1000){
-plazoNumero = parseInt( prompt ('Ingrese dias de plazo, Ej: 30, 60, 90 hasta 360. Solo numeracion '))
+function saludar() {
+const saludo = prompt("Introduzca su nombre y apellido:");
+if(saludo === null || saludo === "") {
+    alert ("Has cancelado o introducido el nombre vacío");
 }else {
-    alert('El monto ingresado es invalido')
+    alert ('Hola ' + saludo)
 }
-if (plazoNumero ==30 ){
-alert ('su interes ganado es de ' + montoDeInversion*tna*plazoNumero/año)}
-else if (plazoNumero ==60){
-    alert ('suinteres ganado es de ' + montoDeInversion*tna*plazoNumero/año)}
-    else if (plazoNumero ==90){
-        alert ('su interes ganado es de  ' + montoDeInversion*tna*plazoNumero/año)}
-    else if (plazoNumero ==120){
-        alert ('su interes ganado es de  ' + montoDeInversion*tna*plazoNumero/año)}
-    else if (plazoNumero ==180){
-        alert ('su interes ganado es de  ' + montoDeInversion*tna*plazoNumero/año)}
-    else if (plazoNumero ==210){
-        alert ('su interes ganado es de ' + montoDeInversion*tna*plazoNumero/año)}
-    
-    else if (plazoNumero ==240){
-        alert ('su interes ganado es de  ' + montoDeInversion*tna*plazoNumero/año)}
-    
-    else if (plazoNumero ==270){
-        alert ('su interes ganado es de ' + montoDeInversion*tna*plazoNumero/año)}
-    else if (plazoNumero ==300){
-            alert ('su interes ganado es  de ' + montoDeInversion*tna*plazoNumero/año)}
-    else if (plazoNumero ==330){
-            alert ('su interes ganado es de ' + montoDeInversion*tna*plazoNumero/año)}
-    else if (plazoNumero ==360){
-            alert ('su interes ganado es de  ' + montoDeInversion*tna*plazoNumero/año)}
-else{
-    alert ('su plazo es invalido')
 }
 } while (montoDeInversionIngresado !== 'ESC') 
 
 
+saludar();
+
+const productos = [ 
+{nombre: 'Iphone 6' , precio: 24000},
+{nombre: 'Iphone 6 Plus' , precio: 28000},
+{nombre: 'Iphone 7' , precio: 30000},
+{nombre: 'Iphone 7 Plus' , precio: 37000},
+{nombre: 'Iphone 8' , precio: 38000},
+{nombre: 'Iphone 8 Plus' , precio: 45000},
+{nombre: 'Iphone X' , precio: 70000},
+{nombre: 'Iphone Xs' , precio: 75000},
+{nombre: 'Iphone Xr' , precio: 80000},
+{nombre: 'Iphone 11' , precio: 90000},
+{nombre: 'Iphone 11 Pro' , precio: 95000},
+{nombre: 'Iphone 11 Pro Max' , precio: 100000},
+{nombre: 'Iphone 12 mini' , precio: 120000},
+{nombre: 'Iphone 12 pro' , precio: 125000},
+{nombre: 'Iphone 12 pro max' , precio: 130000},
+{nombre: 'Iphone 13' , precio: 140000},
+{nombre: 'Iphone 13 mini' , precio: 145000},
+{nombre: 'Iphone 13 Pro' , precio: 150000},
+{nombre: 'Iphone 13 Pro Max' , precio: 155000},
+{nombre: 'Iphone 14' , precio: 160000},
+{nombre: 'Iphone 14 Plus' , precio: 165000},
+{nombre: 'Iphone 14 Pro' , precio: 170000},
+{nombre: 'Iphone 14 Pro Max' , precio: 180000},
+];
