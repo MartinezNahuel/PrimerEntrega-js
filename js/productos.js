05b1801e45dc7ebab8c7c6b09e231b70c1ea96f8
Martinez Nahuel <nahuel.ivan.martinez@hotmail.com>
2023-01-30 16:26:13 -0300

    creando productos en js

---
+++ js/productos.js
@@ -0,0 +1,45 @@
+
+const productos= [
+    {
+        id: 1,
+        nombre: "iphone 12 mini",
+        precio: 100000, 
+        img: "https://cbafederal.net/wp-content/uploads/2021/02/i-12-minii.png",
+    },
+    {
+        id: 2,
+        nombre: "iphone 12 pro max",
+        precio: 110000,
+        img:"https://cbafederal.net/wp-content/uploads/2022/09/Apple-iPhone-14-Pro-Max.jpg",
+    },
+    {
+        id: 3,
+        nombre: "iphone 13",
+        precio: 120000,
+        img: "https://cbafederal.net/wp-content/uploads/2021/10/colores-del-iphone-13-todos-los-tonos-y-tonalidades-que-esperamos-ver-en-el-iphone-13-3.png",
+    },
+    {
+        id: 4,
+        nombre: "iphone 13 pro max",
+        precio: 130000,
+        img: "https://cbafederal.net/wp-content/uploads/2021/10/iphone-13-pro-graphite-select.png",
+    },
+    {
+        id: 5,
+        nombre: "iphone 14 plus",
+        precio: 140000,
+        img: "https://cbafederal.net/wp-content/uploads/2022/11/descarga-22.jpg",
+    },
+    {
+        id: 6,
+        nombre: "iphone 14 pro",
+        precio: 150000,
+        img:"https://cbafederal.net/wp-content/uploads/2022/09/iphone-14-pro-color-morado-oscuro-512-gb-de-apple.jpg",
+    },
+    {
+        id: 7,
+        nombre: "iphone 14 pro max",
+        precio: 160000,
+        img: "https://cbafederal.net/wp-content/uploads/2022/09/Apple-iPhone-14-Pro-Max.jpg"
+    }
+]
